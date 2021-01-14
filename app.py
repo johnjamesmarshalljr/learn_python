2d399d269d2e9ae38c4ebbb62131e96d60194014
jerkzilla <johnjamesmarshalljr@gmail.com>
2021-01-14 16:11:07 -0600

    beginning python stuff

---
+++ app.py
@@ -0,0 +1,21 @@
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/   |")
+
+
+li = ("jj", "mike", "pete")
+
+
+def my_fun(lis):
+    print(lis)
+
+
+my_fun(li)
+
+
+char_name = "John"
+char_age = 32
+
+print("there was a man named {0}".format(char_name))
+print("he was {0} years old".format(char_age))
