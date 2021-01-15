2b8b467a4e47d801fb9bd725461fde3019504469
jerkzilla <johnjamesmarshalljr@gmail.com>
2021-01-15 10:31:40 -0600

    tuple work

---
+++ tuple.py
@@ -0,0 +1,15 @@
+coords = [4, 5]
+coordinates = (4, 5)
+
+print(coordinates[1])
+# print(coords.reverse())
+
+co = ("jj", "ryan", "pete")
+co2 = ["jj", "ryan", "pete"]
+
+pete = co2.pop()
+co2.append("paul")
+print(co2)
+
+co2.append(pete)
+print(co2)
