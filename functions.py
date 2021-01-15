91d277ed87cd25d147c3bcb0ef7d056aba84ac84
jerkzilla <johnjamesmarshalljr@gmail.com>
2021-01-15 10:44:41 -0600

    ifs

--- functions.py
+++ functions.py
@@ -2,5 +2,13 @@ def sayhi(name, age):
     # print(f"hey {name} you are {age} years old")
     print("hey %s you are %s years old" % (name, age))
 
+# sayhi("jj", "34")
 
-sayhi("jj", "34")
+
+def cube(num):
+    return num*num*num
+    # return keyword breaks out of fn
+
+
+result = cube(4)
+print(result)
---
+++ ifs.py
@@ -0,0 +1,12 @@
+is_male = False
+is_tall = True
+
+
+if is_male & is_tall:
+    print("you ar cool")
+elif is_male and not(is_tall):
+    print("u alright")
+elif not(is_male) and is_tall:
+    print("struggle")
+else:
+    print("girl")
