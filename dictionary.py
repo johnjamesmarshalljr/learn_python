21a34038f413ff8e9c9a5b6484f7f2b820b0c655
jerkzilla <johnjamesmarshalljr@gmail.com>
2021-01-16 08:59:03 -0600

    dictionaries

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+monthConversions = {
+    2: "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    5: "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+# print(monthConversions.get("Ded", "NOT VALID"))
+print(monthConversions[5])
+
+# print(monthConversions.get("Ded", "NOT VALID"))
