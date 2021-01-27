0e04640fbdfb549de8852a4e22407553f677eeaa
jerkzilla <johnjamesmarshalljr@gmail.com>
2021-01-26 20:36:08 -0600

    practice

--- FizzBuzz(NAMI).py
+++ FizzBuzz(NAMI).py
@@ -1,9 +1,9 @@
 for num in range(1, 101):
     if num % 15 is 0:
-        print("FizzBuzz")
+        print("buzzfizz")
     elif num % 3 is 0:
-        print("Fizz")
+        print("kizz")
     elif num % 5 is 0:
-        print("Buzz")
+        print("juzz")
     else:
         print(num)
